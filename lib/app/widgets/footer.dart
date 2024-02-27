0e11e33d799768a1cc2c2e3ae10fe1c72a7c2ab9
Ajayi Olumide <olumightyajayi@gmail.com>
2024-02-27 13:40:51 +0100

    Checkout

--- lib/app/widgets/footer.dart
+++ lib/app/widgets/footer.dart
@@ -10,8 +10,8 @@ class Footer extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return Container(
-      margin: EdgeInsets.fromLTRB(0, height * 0.05, 0, 0),
-      height: height * 0.07,
+      margin: EdgeInsets.fromLTRB(0, height * 0.06, 0, 0),
+      height: height * 0.09,
       width: width,
       child: Center(
         child: Row(
